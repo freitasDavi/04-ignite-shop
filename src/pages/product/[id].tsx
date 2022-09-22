c3d54fc04e4dbd21039d86a04df531c250f6e052
Davi <davi.frrs@gmail.com>
2022-09-22 15:02:33 -0300

    Initialize next project + routing

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router"
+
+
+export default function Product() {
+    const { query } = useRouter();
+
+
+    return (
+        <h1>product: {JSON.stringify(query)}</h1>
+    )
+}
